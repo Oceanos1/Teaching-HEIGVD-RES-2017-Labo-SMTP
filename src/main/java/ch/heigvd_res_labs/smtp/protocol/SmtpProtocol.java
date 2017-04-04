ed1fb5bce696162294a0f945de158cc440fa62a3
Michael Spierer <evondom@hotmail.com>
2017-04-04 10:24:07 +0200

    init v2

--- src/main/java/ch/heigvd_res_labs/smtp/protocol/SmtpProtocol.java
+++ src/main/java/ch/heigvd_res_labs/smtp/protocol/SmtpProtocol.java
@@ -1,6 +1,9 @@
+
+package ch.heigvd_res_labs.smpt.protocol;
+
 class SmtpProtocol {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("java.util.logging.SimpleFormatter.format");
 
     }
